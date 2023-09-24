b0f882c5e0aac08e78b0e3c29e68914ea140fb99
Laza <lelcoldplay@gmail.com>
2023-09-24 09:09:29 -0600

    Inserted data in the db and add a security validation on pos function

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -5,8 +5,29 @@ const db = require('../config/database.js'); //importamos nuestra base de datos 
 
 
 
-pokemon.post("/", (req,res,next) =>{
-    return res.status(200).send(req.body);
+pokemon.post("/", async (req,res,next) =>{
+
+    const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+
+    if(pok_name && pok_height && pok_weight && pok_base_experience){
+        let query = "INSERT INTO pokemon(pok_name, pok_height, pok_weight, pok_base_experience)";
+
+        query += `vALUES('${pok_name}', ${pok_height}, ${pok_weight}, ${pok_base_experience})`;
+    
+        const rows = await db.query(query); 
+        
+        if(rows.affectedRows == 1){
+            return res.status(201).json({code: 201, message: "pokemon insertado"});
+        }
+        //En post el 201 es como un 200 en get y dice que el recurso se creo
+        return res.status(500).json({code: 500, message: "Ocurrió un error"});
+
+    }
+
+    res.status(500).json({code: 500, message: "los campos no han sido mandados"});
+
+
+    
 });
 
 /*si solo ponemos nombre es una variable estatica que no nos srive para poner muchos registros
@@ -15,7 +36,7 @@ en este caso para que sea dinamico tenemos que poner /:nombre_de_la_variable
 pokemon.get("/", async (req,res,next) => {
 
     const pkmon = await db.query("SELECT * FROM pokemon");    
-    return res.status(200).json({code:1, message: pkmon});
+    return res.status(200).json({code:200, message: pkmon});
 })
 
 //console.log(req.params.name);
@@ -32,13 +53,12 @@ pokemon.get("/", async (req,res,next) => {
 */
 //Ponemos ([0-9]{1,3}) para dcir que llama a una funcion para admitir solo numeros
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
-    const id = req.params.id -1;
+    const id = req.params.id;
     if(id >= 1 && id <= 722) {
         const pkmon = await db.query("SELECT * FROM pokemon WHERE pok_id = "+id+";");  
-        return res.status(200).json({code: 1, message: pkmon}); //le restamos un 1 para obtener la pso real del arreglo
+        return res.status(200).json({code: 200, message: pkmon}); //le restamos un 1 para obtener la pso real del arreglo
     }
-
-    return res.status(404).send({code: 404, message:"pokemon no encontrado"});
+    return res.status(404).json({code: 404, message: "Pokemon no esta en el rango"});
        
 })
 
@@ -55,7 +75,7 @@ pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
     */
 
    if(pkmon.length > 0){
-    return res.status(200).json({code: 1, message: pkmon}); 
+    return res.status(200).json({code: 200, message: pkmon}); 
     }
     return res.status(404).send({code: 404, message:"pokemon no encontrado"});
 
